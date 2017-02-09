486beb37fb8e7498e50ab4b66cf5d021b8783ca6
Alex Raubach <antigrapist@gmail.com>
2017-02-09 17:19:51 -0600

    built book migration

--- db/migrate/20170209161912_create_books.rb
+++ db/migrate/20170209161912_create_books.rb
@@ -1,4 +1,14 @@
 class CreateBooks < ActiveRecord::Migration
   def change
+    create_table :books do |t|
+      t.integer :isbn
+      t.string :title
+      t.integer :ratings_count
+      t.string :small_image_url
+      t.string :image_url
+      t.integer :average_rating
+      t.string :author
+      t.text :description
+
   end
 end
